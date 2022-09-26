a807b3bb882214231990d3600fa402eef18cea2e
ZinMinTun <kmailsoftwaredevelopment@gmail.com>
2022-09-26 11:36:41 +0630

    Counter

--- resources/views/layouts/menulists/counter_ment.blade.php
+++ resources/views/layouts/menulists/counter_ment.blade.php
@@ -6,15 +6,23 @@
 
                     <li class="{{ Request::is('counter_dashboard') ? 'active' : '' }}">
                         <a href="{{ route('counter_dashboard.index') }}">
-                            <i class="fa fa-users"></i>
+                            <i class="fa fa-home"></i>
                             Dashboard
                         </a>
                     </li>
 
-                    <li class="{{ Request::is('employee') ? 'active' : '' }}">
-                        <a href="{{ route('employee.index') }}">
-                            <i class="fa fa-users"></i>
-                            Employee
+                    <li class="{{ Request::is('order_lists') ? 'active' : '' }}">
+                        <a href="{{ route('order_lists.index') }}">
+                            <i class="fa fa-list"></i>
+                            Order (Processing)
+                        </a>
+                    </li>
+               
+
+                    <li class="{{ Request::is('order_lists') ? 'active' : '' }}" hidden>
+                        <a href="{{ route('order_lists.index') }}">
+                            <i class="fa fa-list"></i>
+                            Complete order
                         </a>
                     </li>
 
